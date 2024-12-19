9a7efd6f9426712fa86260848d3e8f319ab1786f
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-12-19 02:12:08 +0100

    fixing release

--- forge.config.js
+++ forge.config.js
@@ -33,15 +33,15 @@ module.exports = {
             name: '@electron-forge/maker-rpm',
             config: {},
         },
-        {
-            name: '@electron-forge/maker-dmg',
-            config: {
-                name: 'TweakPHP-Installer',
-                icon: './assets/icon.icns',
-                overwrite: true,
-                format: 'UDZO',
-            },
-        },
+        // {
+        //     name: '@electron-forge/maker-dmg',
+        //     config: {
+        //         name: 'TweakPHP-Installer',
+        //         icon: './assets/icon.icns',
+        //         overwrite: true,
+        //         format: 'UDZO',
+        //     },
+        // },
     ],
     plugins: [
         {
