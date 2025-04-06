5e11b8c41b12fa7224ca614f53f7d672d2a2525a
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2025-04-06 18:18:50 -0300

    fix: move click event to button and disable during execution (#138)
    * fix: move click event to button and disable during execution
    
    * lint

--- src/renderer/components/SecondaryButton.vue
+++ src/renderer/components/SecondaryButton.vue
@@ -12,7 +12,7 @@
   <button
     type="button"
     :class="[
-      'inline-flex h-6 text-sm w-max min-w-max items-center justify-center rounded-md border bg-transparent px-4 py-1 text-white transition hover:bg-opacity-50 disabled:opacity-25',
+      'cursor-pointer inline-flex h-6 text-sm w-max min-w-max items-center justify-center rounded-md border bg-transparent px-4 py-1 text-white transition hover:bg-opacity-50 disabled:opacity-25',
       props.class,
     ]"
     :style="{ borderColor: settingsStore.colors.border, color: settingsStore.colors.foreground }"
